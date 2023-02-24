0c3727195b1af6d8d6cf0a7ee17827f78b6d8297
JuanD1206 <76623625+joker120620@users.noreply.github.com>
2023-02-24 10:22:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function ocultar() {
+    document.getElementById("sec_1").setAttribute("class", "seccion_null")
+    document.getElementById("sec_2").setAttribute("class", "seccion_null")
+    document.getElementById("sec_3").setAttribute("class", "seccion_null")
+    
+}
+function mostrar(n) {
+    ocultar()
+    document.getElementById(`sec_${n}`).setAttribute("class", "seccion_active")
+    
+}
+function cerrarModal(n){
+  mostrar(n)
+  let Modal=document.getElementById('modal')
+  Modal.setAttribute('class', 'modal_oculto')
+}
+let btnModal=document.getElementById('abrirModal')
+function abrirModal(){
+  ocultar()
+  let Modal=document.getElementById('modal')
+  Modal.setAttribute('class', 'seccion_active')
+}
+
